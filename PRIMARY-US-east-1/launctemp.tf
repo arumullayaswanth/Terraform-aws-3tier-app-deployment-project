43c6c6de514443c23ccdea834be4500e5891c005
Arumulla Yaswanth Reddy <164642237+arumullayaswanth@users.noreply.github.com>
2025-06-07 11:23:07 +0530

    Update launctemp.tf

--- PRIMARY-US-east-1/launctemp.tf
+++ PRIMARY-US-east-1/launctemp.tf
@@ -17,7 +17,7 @@ resource "aws_launch_template" "frontend" {
   image_id = data.aws_ami.example.id
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.frontend-server-sg.id]
-  key_name = "us-east-1" #chnage the key 
+  key_name = var.key-name 
   user_data = filebase64("${path.module}/frontend-lt.sh")
   #default_version = 1
   update_default_version = true
@@ -50,7 +50,7 @@ resource "aws_launch_template" "backend" {
   image_id = data.aws_ami.example1.id
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.backend-server-sg.id]
-  key_name = "us-east-1" 
+  key_name = var.key-name
   user_data = filebase64("${path.module}/backend-lt.sh")
   #default_version = 1
   update_default_version = true
